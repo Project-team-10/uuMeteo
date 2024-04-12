600d90889d7f6638178ac00b3d23d84134af3428
otavlna <otavlna@protonmail.ch>
2024-04-12 21:27:17 +0200

    Install cors middlware

--- iot server/app.js
+++ iot server/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to a SQLite database.
